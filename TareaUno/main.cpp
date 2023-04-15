93187c5a592c7917978efde4cbe2cb0ab0e4a522
FabianSander1 <127806595+FabianSander1@users.noreply.github.com>
2023-04-15 13:12:23 -0600

    Update main.cpp

--- TareaUno/main.cpp
+++ TareaUno/main.cpp
@@ -2,8 +2,12 @@
 #include <vector>
 #include <string>
 
-//Me di cuenta de que tengo que hacer primero las clases, en este caso se hicieron las clases Aula, Edificio, Finca y Campus. LE pongo un Hola mundo para ver que complie bien y que corra.
-//Mas adelante le voy a hacer la funcuin que imprima la info que le voy a meter en una funion main. MAs adelante subo el archivo hecho a mano. 
+// Se hizo la funcion que imprime toda la informacion del campus
+// Todavia falta insertar toda la info del esquematico del pdf.
+// Tengo una idea de que se va a poner dentro de la funcion main. Con diferentes parametros para los pupitres, proyectores, pupitres, booleanos para el bus y para el proyector y ascensor.
+
+
+
 
 
 class Aula {
@@ -58,6 +62,63 @@ public:
         fincas.push_back(finca);
     }
 };
+
+void imprimirInfoCampus(const Campus& campus);
+
+
+
+
+void imprimirInfoCampus(const Campus& campus) {
+    std::cout << "Campus: " << campus.nombre << std::endl;
+
+    for (const Finca& finca : campus.fincas) {
+        std::cout << "Finca " << finca.id << std::endl;
+
+        int edificiosAulas = 0;
+        int edificiosParqueo = 0;
+        for (const Edificio& edificio : finca.edificios) {
+            if (edificio.tipo == "Aulas") {
+                edificiosAulas++;
+            } else if (edificio.tipo == "Parqueo") {
+                edificiosParqueo++;
+            }
+        }
+
+        std::cout << "En la finca " << finca.id << " hay " << edificiosAulas << " edificios de aulas y " << edificiosParqueo << " de parqueos." << std::endl;
+
+        std::cout << "El bus pasa por los edificios:";
+        for (const Edificio& edificio : finca.edificios) {
+            if (edificio.bus) {
+                std::cout << " " << edificio.id;
+            }
+        }
+        std::cout << "." << std::endl;
+
+        for (const Edificio& edificio : finca.edificios) {
+            std::cout << "El edificio " << edificio.id << " tiene " << edificio.aulas.size() << " aulas";
+            if (edificio.tipo == "Parqueo") {
+                std::cout << " y " << edificio.parqueos << " espacios de parqueo";
+            }
+            std::cout << "." << std::endl;
+
+            for (const Aula& aula : edificio.aulas) {
+                std::cout << "El aula " << aula.id << " tiene " << aula.pupitres << " pupitres";
+                if (aula.proyector) {
+                    std::cout << " y un proyector";
+                }
+                std::cout << "." << std::endl;
+            }
+
+            if (edificio.ascensor) {
+                std::cout << "El edificio " << edificio.id << " tiene un ascensor." << std::endl;
+            }
+
+            if (edificio.soda) {
+                std::cout << "El edificio " << edificio.id << " tiene una soda." << std::endl;
+            }
+        }
+    }
+}
 
 
 int main() {
