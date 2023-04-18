483ba344171942cef82bfe033e9d0ec4659f2b70
FabianSander1 <127806595+FabianSander1@users.noreply.github.com>
2023-04-18 17:57:19 -0600

    Update pruebas.hpp

--- TareaUno/pruebas.hpp
+++ TareaUno/pruebas.hpp
@@ -1,3 +1,27 @@
+/*
+* Copyright (c) 2023, Fabian Salberto Sander Hangen (@FabianSander1)
+*
+* Se concede permiso, de forma gratuita, a cualquier persona que obtenga una
+* copia de este software y de los archivos de documentación asociados (el "Software"),
+* para utilizar el Software sin restricciones, incluyendo, sin limitación,
+* los derechos de uso, copia, modificación, fusión, publicación, distribución,
+* sublicencia y/o venta de copias del Software, y para permitir que las personas
+* a las que se les proporcione el Software hagan lo mismo, con sujeción a las siguientes condiciones:
+*
+* El aviso de copyright anterior y este aviso de permiso se incluirán en todas
+* las copias o partes sustanciales del Software.
+*
+* EL SOFTWARE SE PROPORCIONA "TAL CUAL", SIN GARANTÍA DE NINGÚN TIPO, EXPRESA O
+* IMPLÍCITA, INCLUYENDO, PERO NO LIMITADO A, LAS GARANTÍAS DE COMERCIABILIDAD, 
+* APTITUD PARA UN PROPÓSITO PARTICULAR Y NO INFRACCIÓN. EN NINGÚN CASO LOS 
+* TITULARES DE LOS DERECHOS DE AUTOR SERÁN RESPONSABLES POR CUALQUIER RECLAMO, 
+* DAÑO U OTRA RESPONSABILIDAD, YA SEA EN UNA ACCIÓN CONTRACTUAL, AGRAVIO O 
+* CUALQUIER OTRO MOTIVO, DERIVADO DE, DE O EN RELACIÓN CON EL SOFTWARE O EL 
+* USO U OTROS NEGOCIOS EN EL SOFTWARE.
+*/
+
+
+
 #include <iostream>
 #include <vector>
 #include <string>
