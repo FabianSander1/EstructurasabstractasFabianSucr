48c413b4f831887584dcba90f74bd5eda8ce1808
FabianSander1 <127806595+FabianSander1@users.noreply.github.com>
2023-05-15 11:47:23 -0600

    Update main.cpp

--- Tarea3/main.cpp
+++ Tarea3/main.cpp
@@ -6,20 +6,35 @@
 
 using namespace std;
 
+/**
+ * @class Empleado
+ * @brief Clase que representa a un empleado.
+ */
 class Empleado {
 public:
-    string nombre;
-    string correo;
-    int edad;
-    string departamento;
-    double salario;
+    string nombre;          ///< Nombre del empleado.
+    string correo;          ///< Correo electrónico del empleado.
+    int edad;               ///< Edad del empleado.
+    string departamento;    ///< Departamento del empleado.
+    double salario;         ///< Salario del empleado.
 };
 
+/**
+ * @brief Función para validar una entrada con un patrón dado.
+ * @param entrada La entrada a validar.
+ * @param patron El patrón de validación.
+ * @return true si la entrada es válida, false en caso contrario.
+ */
 bool validarEntrada(string entrada, string patron) {
     regex reg(patron);
     return regex_match(entrada, reg);
 }
 
+/**
+ * @brief Función para leer los datos de empleados desde un archivo.
+ * @param nombreArchivo El nombre del archivo que contiene los datos.
+ * @return Un vector de objetos Empleado con los empleados leídos.
+ */
 vector<Empleado> leerArchivo(string nombreArchivo) {
     vector<Empleado> empleados;
     ifstream archivo(nombreArchivo);
@@ -57,6 +72,12 @@ vector<Empleado> leerArchivo(string nombreArchivo) {
     return empleados;
 }
 
+/**
+ * @brief Función para buscar empleados por departamento.
+ * @param empleados El vector de empleados en el que realizar la búsqueda.
+ * @param departamento El departamento a buscar.
+ * @return Un vector de empleados que pertenecen al departamento buscado.
+ */
 vector<Empleado> buscarPorDepartamento(vector<Empleado> empleados, string departamento) {
     vector<Empleado> resultado;
 
@@ -69,6 +90,13 @@ vector<Empleado> buscarPorDepartamento(vector<Empleado> empleados, string depart
     return resultado;
 }
 
+/**
+ * @brief Función para buscar empleados por rango de salario.
+ * @param empleados El vector de empleados en el que realizar la búsqueda.
+ * @param salarioMinimo El salario mínimo del rango.
+ * @param salarioMaximo El salario máximo del rango.
+ * @return Un vector de empleados cuyos salarios están dentro del rango especificado.
+ */
 vector<Empleado> buscarPorSalario(vector<Empleado> empleados, double salarioMinimo, double salarioMaximo) {
     vector<Empleado> resultado;
 
@@ -81,8 +109,12 @@ vector<Empleado> buscarPorSalario(vector<Empleado> empleados, double salarioMini
     return resultado;
 }
 
+/**
+ * @brief Función principal.
+ */
+
 int main() {
-    string archivo = "Empleados.txt";
+    string archivo = "Empleados.csv";
     vector<Empleado> empleados = leerArchivo(archivo);
 
     // Ejemplo de búsqueda por departamento
@@ -162,3 +194,26 @@ int main() {
 
     return 0;
 }
+
+
+/*
+* Copyright (c) 2023, Fabian Salberto Sander Hangen (@FabianSander1)
+*
+* Se concede permiso, de forma gratuita, a cualquier persona que obtenga una
+* copia de este software y de los archivos de documentación asociados (el "Software"),
+* para utilizar el Software sin restricciones, incluyendo, sin limitación,
+* los derechos de uso, copia, modificación, fusión, publicación, distribución,
+* sublicencia y/o venta de copias del Software, y para permitir que las personas
+* a las que se les proporcione el Software hagan lo mismo, con sujeción a las siguientes condiciones:
+*
+* El aviso de copyright anterior y este aviso de permiso se incluirán en todas
+* las copias o partes sustanciales del Software.
+*
+* EL SOFTWARE SE PROPORCIONA "TAL CUAL", SIN GARANTÍA DE NINGÚN TIPO, EXPRESA O
+* IMPLÍCITA, INCLUYENDO, PERO NO LIMITADO A, LAS GARANTÍAS DE COMERCIABILIDAD, 
+* APTITUD PARA UN PROPÓSITO PARTICULAR Y NO INFRACCIÓN. EN NINGÚN CASO LOS 
+* TITULARES DE LOS DERECHOS DE AUTOR SERÁN RESPONSABLES POR CUALQUIER RECLAMO, 
+* DAÑO U OTRA RESPONSABILIDAD, YA SEA EN UNA ACCIÓN CONTRACTUAL, AGRAVIO O 
+* CUALQUIER OTRO MOTIVO, DERIVADO DE, DE O EN RELACIÓN CON EL SOFTWARE O EL 
+* USO U OTROS NEGOCIOS EN EL SOFTWARE.
+*/
