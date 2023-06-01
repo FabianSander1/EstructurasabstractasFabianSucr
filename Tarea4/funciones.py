e26de99c153b898015099a64204ea5460384ef5d
Fabian Sander <fsanderh@gmail.com>
2023-06-01 12:25:05 -0600

    version con filtros para las demas columnas
    Signed-off-by: Fabian Sander <fsanderh@gmail.com>

--- Tarea4/funciones.py
+++ Tarea4/funciones.py
@@ -3,6 +3,32 @@ import re
 from clases import Empleado
 
 
+# Funcion que verifica si el nombre contiene dos partes
+# y cada una es alfabética
+def nombre_valido(nombre):
+    partes = nombre.split()
+    return len(partes) == 2 and all(parte.isalpha() for parte in partes)
+
+
+# Funcion que verifica si la edad es un número entero
+def edad_valida(edad):
+    return edad.isdigit()
+
+
+# Funcion que verifica si el departamento es alfabético
+def departamento_valido(departamento):
+    return departamento.isalpha()
+
+
+# Funcion que verifica si el salario es un número entero o flotante
+def salario_valido(salario):
+    try:
+        float(salario)
+        return True
+    except ValueError:
+        return False
+
+
 # Función para validar el formato del correo. Retorna True si el correo
 # es válido, de lo contrario retorna False.
 def correo_valido(correo):
@@ -17,14 +43,27 @@ def leer_archivo(nombre_archivo):
     empleados = []
     with open(nombre_archivo, newline='') as archivo:
         reader = csv.reader(archivo)
-        next(reader)  # Saltar encabezado
+        next(reader)
         for row in reader:
             if len(row) == 5:
                 nombre, correo, edad, departamento, salario = row
+                if not nombre_valido(nombre):
+                    nombre = "Nombre invalido"
                 if not correo_valido(correo):
                     correo = "Correo invalido"
-                empleado = Empleado(nombre, correo, int(edad),
-                                    departamento, float(salario))
+                if not edad_valida(edad):
+                    edad = "Edad invalida"
+                else:
+                    edad = int(edad)
+                if not departamento_valido(departamento):
+                    departamento = "Departamento invalido"
+                if not salario_valido(salario):
+                    salario = "Salario invalido"
+                else:
+                    salario = float(salario)
+
+                empleado = Empleado(nombre,
+                                    correo, edad, departamento, salario)
                 empleados.append(empleado)
             else:
                 print(f"Omitiendo fila con datos incompletos: {row}")
